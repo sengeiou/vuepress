94c79facc36e9dd8c38eb5752ed9ba41de1cdd91
tienouc <willtien@outlook.com>
2020-04-19 00:56:55 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,29 @@
 module.exports = {
-  base:'/vuepress/',
+  base: '/vuepress/',
   title: "Tien VuePress",
   description: 'Keep hungry, Keep foolish',
   head: [
     ['meta', { name: 'author', content: 'Tien' }],
     ['meta', { name: 'keywords', content: 'vuepress,Tien,vuepress介绍' }],
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  plugins: [
+    '@vuepress/pwa', , {
+      serviceWorker: true,
+      updatePopup: true
+    },
+    '@vuepress/back-to-top'
   ],
   themeConfig: {
+    smoothScroll: true,
     lastUpdated: 'Last Updated',
     logo: '/assets/img/logo.png',
     sidebar: [
