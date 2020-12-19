8a2c4db3645176dc5a8bc82e0f72432d39f3078b
tienouc <willtien@outlook.com>
2020-12-19 20:45:04 +0800

    Github Actions

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,10 +22,8 @@ module.exports = {
         subSidebar: 'auto',
         authorAvatar: '/assets/img/avatar.jpg',
         valineConfig: {
-            // appId: secret.appID,// your appId
-            // appKey: secret.appKey, // your appKey
-            appId: process.env.VALINE_APPID,
-            appKey: process.env.VALINE_APPKEY
+            appId: `'${{ process.env.VALINE_APPID }}'`,
+            appKey: `'${{ process.env.VALINE_APPKEY }}'`
         },
         // vssueConfig: {
         //     platform: 'github-v4',
